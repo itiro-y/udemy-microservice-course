9a52e8695b4490e01834e2cf6d53563f8ca8cdbc
Ayrton Yoshii <itiro_yoshii@hotmail.com>
2025-07-31 16:46:06 -0300

    First Commit. Combining all microservices repositories

---
+++ api-gateway/src/main/java/br/com/erudio/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package br.com.erudio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+// Deixando o Requests feitas pelo Swagger-UI forem aceitas como Cors
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
+        corsConfig.setAllowedMethods(Arrays.asList("*"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setMaxAge(3600L);
+        corsConfig.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return source;
+    }
+}
