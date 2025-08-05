bbbb0a401634e9d6f0913070c4ac64dc8b8b6aa0
Ayrton Yoshii <itiro_yoshii@hotmail.com>
2025-08-05 19:44:59 -0300

    Enhance health management and logging in book and exchange services

--- book-service/src/main/java/br/com/erudio/proxy/ExchangeProxy.java
+++ book-service/src/main/java/br/com/erudio/proxy/ExchangeProxy.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.math.BigDecimal;
 
-@FeignClient(name = "exchange-service")
+//@FeignClient(name = "exchange-service")
+//EXCHANGE_SERVICE_SERVICE_HOST - kubernetes cria com este nome
+@FeignClient(name = "exchange-service", url = "${EXCHANGE_SERVICE_SERVICE_HOST:http://host.docker.internal}:8000")
 public interface ExchangeProxy {
 
     @GetMapping(value = "/exchange-service/{amount}/{from}/{to}")
